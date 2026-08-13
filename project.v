8689e1265790206ddd127b2563f50058dceee519
Muhammad Rafi <61513614+rafiwew@users.noreply.github.com>
2026-08-13 11:34:41 +0700

    Update project.v

--- project.v
+++ project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_serdes (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,25 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+ 
+    
+    wire ser_in = uio_in[0];
+    wire ser_out = uio_out[0];
+    wire data_en = uio_in[2];
+    wire par_en = uio_in[3];
+    wire ser_en = uio_in[4];
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+    serdes_top serdes_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .data_en(data_en),
+        .ser_in(ser_in),
+        .par_en(par_en),  
+        .ser_en(ser_en),  
+        .ser_out(ser_out),
+        .data_out(uo_out),
+        .data_8b_in(ui_in)  
+    );
+      assign uio_oe = 8'b0;  
+    assign uio_out[7:1] = 7'b0;
 endmodule
